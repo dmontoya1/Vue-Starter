907f8759ce615f3797385df454e2e9ac2bfb98e7
Daniel Montoya <dmontoya.lab@gmail.com>
2021-02-11 16:53:51 -0500

    First commit

---
+++ src/components/CarComposition.vue
@@ -0,0 +1,38 @@
+<template>
+  <h2>Automóvil - Composition</h2>
+  <p>Marca: {{ brand }}</p>
+  <p>Modelo: {{ model }}</p>
+  <p>Potencia: {{ power }}</p>
+
+  <button @click="upPower">Aumentar</button>
+  <button @click="downPower(20)">Disminuir</button>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const brand = "Audi";
+    const model = "A4";
+    let power = ref(40);
+
+    const upPower = () => {
+      power.value += 1;
+    };
+
+    const downPower = (newPower) => {
+      power.value -= newPower;
+    };
+
+    return {
+      brand,
+      model,
+      power,
+      upPower,
+      downPower,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/CarOptions.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>Automóvil - Options</h2>
+  <p>Marca: {{ brand }}</p>
+  <p>Modelo: {{ model }}</p>
+  <p>Potencia: {{ power }}</p>
+
+  <button @click="upPower">Aumentar</button>
+  <button @click="downPower">Disminuir</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      brand: "Ford",
+      model: "Fiesta",
+      power: 160,
+    };
+  },
+  methods: {
+    upPower() {
+      this.power += 1;
+    },
+    downPower() {
+      this.power -= 1;
+    },
+  },
+};
+</script>
+
+<style></style>
